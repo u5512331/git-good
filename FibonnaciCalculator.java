bb3104602805d12237f8cb5c7b6ea493202e297c
Dante Faustino do Nascimento <Dante.Faustino-Do-Nascimento@warwick.ac.uk>
2023-10-03 17:20:19 +0100

    remove misnamed files

--- FibonnaciCalculator.java
+++
@@ -1,8 +0,0 @@
-public class FibonacciCalculator {
-    public int calculate(int n) {
-        if (n <= 0 || n == 1)
-            return n;
-        else
-            return calculate(n - 1) + calculate(n - 2);
-    }
-}
